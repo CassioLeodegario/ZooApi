f5500ad5d99aefd013c1b765ae2e8e3629d99715
CassioLeodegario <cassio.leodegario@gmail.com>
2021-10-04 22:28:18 -0300

    feat: add animal domain

---
+++ domain/animal.go
@@ -0,0 +1,81 @@
+package domain
+
+import (
+	"github.com/asaskevich/govalidator"
+)
+
+func init() {
+
+}
+
+func init() {
+	govalidator.SetFieldsRequiredByDefault(true)
+}
+
+type AnimalInterface interface {
+	IsValid() (bool, error)
+	GetID() string
+	GetName() string
+	GetSpecie() Species
+	GetDiet() string
+}
+
+type AnimalServiceInterface interface {
+	GetAll() ([]AnimalInterface, error)
+	Get(id string) (AnimalInterface, error)
+	Create(animal AnimalInterface) (AnimalInterface, error)
+	Update(animal AnimalInterface) (AnimalInterface, error)
+	Delete(animal Animal) error
+}
+
+type AnimalReader interface {
+	get(id string) (AnimalInterface, error)
+}
+
+type AnimalWriter interface {
+	Save(animal AnimalInterface) (AnimalInterface, error)
+}
+
+type AnimalPersistenceInterface interface {
+	AnimalReader
+	AnimalWriter
+}
+
+type Animal struct {
+	ID      string   `valid:"uuidv4"`
+	Name    string   `valid:"required"`
+	Species *Species `valid:"required"`
+	Diet    string   `valid:"string,optional"`
+}
+
+// Species Model
+type Species struct {
+	Name          string `valid:"required"`
+	Family        string `valid:"required"`
+	Domestication string `valid:"required"`
+	Endangered    string `valid:"required"`
+}
+
+func (a *Animal) IsValid() (bool, error) {
+	_, err := govalidator.ValidateStruct(a)
+	if err != nil {
+		return false, err
+	}
+	return true, nil
+}
+
+func (a *Animal) GetID() string {
+	return a.ID
+}
+
+func (a *Animal) GetName() string {
+	return a.Name
+}
+
+func (a *Animal) GetSpecie() Species {
+	return *a.Species
+}
+
+func (a *Animal) GetDiet() string {
+	return a.Diet
+}
